73fc3ce777aa216dfb5e69384144d2fbcc0ff98a
deonopinallae <deonopinallae@yandex.ru>
2025-07-30 18:32:13 +0300

    настроить проект

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const AppStyles = styled.div`
+  text-align: center;
+`
+
+
+export const App = () => {
+  return (
+    <AppStyles>
+      <i className="fa fa-bath" aria-hidden="true"></i>
+      <div>123</div>
+    </AppStyles>
+  )
+}
+
